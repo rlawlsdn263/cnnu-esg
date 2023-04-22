872fe9dc85241becdc168e15873d6ed24a1c2216
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-22 17:03:13 +0900

    [✨기능] 컴포넌트에 데이터 바인딩

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,30 +1,46 @@
 import styles from "./Test.module.css";
 import {Button} from "../../components"
 import data from "../../data/data.json"
-
-function no(){
-  console.log(data)
-  console.log(-1);
-}
-
-function normal(){
-  console.log(0);
-}
-
-function yes(){
-  console.log(1);
-}
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export function Test() {
+  const [test, setTest] = useState(data);
+  const [count, setCount] = useState(0);
+  /* useEffect(() => {
+    getCount();
+  }, []); */
+
+  /* function keepCount() {
+    localStorage.setItem("count", count);
+  }
+
+  function getCount() {
+    let savedCount = JSON.stringify(localStorage.getItem("count"));
+    setCount(savedCount);
+  } */
+
+  function no(){
+    setCount(prev => prev + 1);
+  }
+  
+  function normal(){
+    setCount(prev => prev + 2);
+  }
+  
+  function yes(){
+    setCount(prev => prev + 3);
+  }
+
   return(<>
     <div className={styles.test}>
       <div className={styles.testHeader}>
-        <span className={styles.name}>STEP 02 환경(E) 대응</span>
-        <span className={styles.number}>3/10</span>
+        <span className={styles.name}>{test[count].section}</span>
+        <span className={styles.number}>{test[count].id}/{test.length}</span>
       </div>
       <div className={styles.testBody}>
-        <span className={styles.testNumber}>Q3.</span>
-        <p className={styles.testText}>우리 회사는 산림 벌채 및/또는 자연 생태계 전환을 줄이고, 산림 환경 활성화를 위해 사유림 소유자와 협력하고 있다. </p>
+        <span className={styles.testNumber}>Q{test[count].id}.</span>
+        <p className={styles.testText}>{test[count].content}</p>
       </div>
       <div className={styles.testButtons}>
         <Button className={styles.ButtonNo} onClick={no}>그렇지 않다</Button>
