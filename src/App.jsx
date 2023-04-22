af496547aec9d408fec67d5761d5e4b61fefc51e
rlawlsdn263 <rlawlsdn263@naver.com>
2023-04-22 15:22:15 +0900

    [✨기능] 라우터 경로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Button } from "./components"
+import { Home, Test } from "./pages"
 import logo from "./assets/logo.svg"
 import styles from "./App.module.css"
-import { handleClick } from './functions/_index.js'
+import {Routes, Route} from "react-router-dom";
 
 function App() {
 
@@ -15,13 +15,10 @@ function App() {
         </h1>
       </header>
       <div className={styles.background}>
-        <div className={styles.container}>
-          <p className={styles.title}>산림ESG경영 자가진단 서비스</p>
-          <p className={styles.text}>산림ESG경영 자가진단 서비스는 기업의 산림분야 <strong>ESG 수준을 진단할 수 있는 자가진단 서비스</strong>를 제공합니다.</p>
-          <p className={styles.text}>귀사의 산림분야 ESG에 대한 현재 수준을 진단하여 ESG 역량을 제고하고 개선할 수 있도록 개발되었습니다.</p>
-          <p className={styles.text}>자가진단을 통해 산림ESG 경영을 위한 기본적인 요건들을 확인하실 수 있습니다.</p>
-        </div>
-        <Button className={styles.button} onClick={handleClick}>자가진단 시작하기</Button>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/test" element={<Test />}/>
+        </Routes>
         <div className={styles.shadow}></div>
       </div>
       <footer className={styles.footer}>
