16a827ba19a228ca1f3f9d8e71995f2b309df4f3
rlawlsdn263 <rlawlsdn263@naver.com>
2023-11-13 22:47:59 +0900

    로고 링크주소 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { Home, Test, Result } from "./pages"
-import logo from "./assets/logo.svg"
-import styles from "./App.module.css"
-import {Routes, Route} from "react-router-dom";
+import { Home, Test, Result } from "./pages";
+import logo from "./assets/logo.svg";
+import styles from "./App.module.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <div>
       <header className={styles.header}>
         <h1 className={styles.logo}>
-          <a href="/">
+          <a href="https://rlawlsdn263.github.io/cnnu-esg/#/">
             <img src={logo} alt="전남대학교"></img>
           </a>
         </h1>
       </header>
       <div className={styles.background}>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/test" element={<Test />}/>
-          <Route path="/result" element={<Result />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/result" element={<Result />} />
         </Routes>
         <div className={styles.shadow}></div>
       </div>
       <footer className={styles.footer}>
-        <span className={styles.copyright}>Copyright© 2023 CHONNAM NATIONAL UNIVERSITY. All rights reserved.</span>
-        <span className={styles.license}><a href="http://www.freepik.com">Designed by rawpixel.com / Freepik</a></span>
+        <span className={styles.copyright}>
+          Copyright© 2023 CHONNAM NATIONAL UNIVERSITY. All rights reserved.
+        </span>
+        <span className={styles.license}>
+          <a href="http://www.freepik.com">
+            Designed by rawpixel.com / Freepik
+          </a>
+        </span>
       </footer>
     </div>
-    )
+  );
 }
 
-export default App
+export default App;
